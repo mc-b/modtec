bb6040ff63baae48b7ea90dd8fc38b46955ea9b6
mc-b <marcel.bernet@ch-open.ch>
2022-07-28 11:47:59 +0200

    Update BPMN Prozess findet via Jupyter Notebook statt

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,26 +16,6 @@ kubectl apply -f https://raw.githubusercontent.com/mc-b/iot.kafka/master/iot-kaf
 kubectl apply -f https://raw.githubusercontent.com/mc-b/iot.kafka/master/iot-kafka-consumer.yaml
 kubectl apply -f https://raw.githubusercontent.com/mc-b/iot.kafka/master/iot-kafka-pipe.yaml
 
-# BPMN Umgebung und Upload BPMN Prozess
-sudo docker pull camunda/camunda-bpm-platform
+# BPMN Umgebung. Upload BPMN Prozess findet via Jupyter Notebook statt
 kubectl apply -f https://raw.githubusercontent.com/mc-b/misegr/master/bpmn/camunda.yaml
 
-cd /vagrant
-wget https://raw.githubusercontent.com/mc-b/misegr/master/bpmn/RechnungStep3.bpmn -O RechnungStep3.bpmn
-
-sleep 30
-for i in {1..150}; do # timeout for 5 minutes
-   curl -k https://localhost:30443/engine-rest/deployment &> /dev/null
-   if [ $? -eq 0 ]; then
-      break
-  fi
-  sleep 2
-done
-
-curl -k -w "\n" \
--H "Accept: application/json" \
--F "deployment-name=rechnung" \
--F "enable-duplicate-filtering=true" \
--F "deploy-changed-only=true" \
--F "Rechnung.bpmn=@RechnungStep3.bpmn" \
-https://localhost:30443/engine-rest/deployment/create
