ab4281cf2cd08032a2c94e51b22828a67baa5ab6
mc-b <marcel.bernet@ch-open.ch>
2024-07-26 19:07:57 +0200

    weniger Ressourcen

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ module "master" {
   userdata    = "cloud-init-modtec.yaml"
 
   cores   = 4
-  memory  = 16
+  memory  = 8
   storage = 32
   # SSH, Kubernetes, NFS
   ports      = [ 22, 80, 16443, 25000 ]
